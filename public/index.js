5a2d052727614c83311898f7446e40a8b7097a7c
shay_daniel <shay@isom.org>
2020-10-20 17:30:24 -0700

    styling

--- public/index.js
+++ public/index.js
@@ -124,7 +124,7 @@ const renderNoteList = (notes) => {
   };
 
   if (notes.length === 0) {
-    noteListItems.push(create$li("No saved Notes", false));
+    noteListItems.push(create$li("no saved notes", false));
   }
 
   notes.forEach((note) => {
